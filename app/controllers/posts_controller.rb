db1d0e41233ebf0cb68d6028e70da665782c98c4
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-04-16 18:24:42 +0300

    feat: ctreating a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,29 @@
 class PostsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
 
 
   def index
 
   end
 
-  def new
 
-  end
 
-  def create
-    
+ def create
+  post = Post.new(post_params)
+  if post_params.blank?
+    render json: { success: false, message: "Fill in the required fields" }, status: :unprocessable_entity
+  elsif post.save
+    render json: { success: true, message: "Your post has been created", data: post }, status: :created
+  else
+    render json: { success: false, message: "An error has occurred" }, status: :unprocessable_entity
   end
+end
+
+
+
+  private
+
+def post_params
+  params.require(:post).permit(:title,  :content,)
+end
 end
